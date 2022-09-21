583d50e157a4bd20ca7aca9eac51946cedb8f3e6
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 13:10:20 +0600

    3.1

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,5 +1,14 @@
+import java.util.*
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println ("Today is $day and the fish eat $food")
+}
 fun main(args: Array<String>) {
-    val temperature = 10
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
-    println(message)
+    feedTheFish()
+}
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
 }
