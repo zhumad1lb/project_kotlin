aeadf3c3365a649f75b9b4eacdbac7cbfab7d74b
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 13:34:14 +0600

    4.2

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,9 +1,34 @@
-import java.util.*
-fun swim(speed: String = "fast") {
-    println("swimming $speed")
-}
-fun main(args: Array<String>){
-    swim()   // uses default speed
-    swim("slow")   // positional argument
-    swim(speed="turtle-like")   // named parameter
-}
+ import java.util.*
+ fun main(args: Array<String>) {
+     feedTheFish()
+ }
+ fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+     return when {
+         temperature > 30 -> true
+         dirty > 30 -> true
+         day == "Sunday" ->  true
+         else -> false
+     }
+ }
+
+ fun feedTheFish() {
+     val day = randomDay()
+     val food = fishFood(day)
+     println ("Today is $day and the fish eat $food")
+     println("Change water: ${shouldChangeWater(day)}")
+ }
+  fun randomDay() : String {
+      val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+          "Friday", "Saturday", "Sunday")
+      return week[Random().nextInt(week.size)]
+  }
+  fun fishFood (day : String) : String {
+      return when (day) {
+          "Monday" -> "flakes"
+          "Wednesday" -> "redworms"
+          "Thursday" -> "granules"
+          "Friday" -> "mosquitoes"
+          "Sunday" -> "plankton"
+          else -> "nothing"
+      }
+  }
