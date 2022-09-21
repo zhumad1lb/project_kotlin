db361f53ecfa747e31977fdb17d91b1c18ed931f
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 16:20:36 +0600

    6.5

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,8 +1,9 @@
-fun main() {
-    var dirtyLevel = 20
-    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
-    println(updateDirty(30, waterFilter))
-}
-fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
-    return operation(dirty)
-}
+ fun main() {
+
+     println(updateDirty(15, ::increaseDirty))
+ }
+
+ fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+     return operation(dirty)
+ }
+ fun increaseDirty( start: Int ) = start + 1
