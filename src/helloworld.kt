5d3719451771e78f2c7e62e031c314f2ce3536cd
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 13:49:40 +0600

    5.2

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,4 +1,13 @@
 fun main() {
-    println( decorations.filter {it[0] == 'p'})
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    // eager, creates a new list
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+    // lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+    // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
 }
-val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
