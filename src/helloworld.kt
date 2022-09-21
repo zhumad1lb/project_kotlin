693e854866d73b5229e9070d01ef681c250bdddd
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 13:43:06 +0600

    5.1

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,38 +1,4 @@
- import java.util.*
- fun main(args: Array<String>) {
-     feedTheFish()
- }
- fun isTooHot(temperature: Int) = temperature > 30
-
- fun isDirty(dirty: Int) = dirty > 30
-
- fun isSunday(day: String) = day == "Sunday"
- fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-     return when {
-         isTooHot(temperature) -> true
-         isDirty(dirty) -> true
-         isSunday(day) -> true
-         else  -> false
-     }
- }
- fun feedTheFish() {
-     val day = randomDay()
-     val food = fishFood(day)
-     println ("Today is $day and the fish eat $food")
-     println("Change water: ${shouldChangeWater(day)}")
- }
-  fun randomDay() : String {
-      val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-          "Friday", "Saturday", "Sunday")
-      return week[Random().nextInt(week.size)]
-  }
-  fun fishFood (day : String) : String {
-      return when (day) {
-          "Monday" -> "flakes"
-          "Wednesday" -> "redworms"
-          "Thursday" -> "granules"
-          "Friday" -> "mosquitoes"
-          "Sunday" -> "plankton"
-          else -> "nothing"
-      }
-  }
+fun main() {
+    println( decorations.filter {it[0] == 'p'})
+}
+val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
