022ba8dbec348785da2f401c9d280893ff717da7
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 12:43:55 +0600

    2.3

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,4 +1,5 @@
 fun main(args: Array<String>) {
-    val isUnit = println("This is an expression")
-    println(isUnit)
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
 }
