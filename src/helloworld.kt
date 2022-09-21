bb6d01de2f1c0bf6428659c8f29e852ef801c8a6
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 14:26:52 +0600

    5.2.8

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,12 +1,9 @@
 fun main() {
     val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
-    val lazyMap = decorations.asSequence().map {
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
         println("access: $it")
         it
     }
-    println("lazy: $lazyMap")
     println("-----")
-    println("first: ${lazyMap.first()}")
-    println("-----")
-    println("all: ${lazyMap.toList()}")
+    println("filtered: ${lazyMap2.toList()}")
 }
