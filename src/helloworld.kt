62c0ec4c0bac0a97612d9336881b3e678b78c6b6
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 12:46:02 +0600

    2.5

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val temperature = 10
-    val isHot = if (temperature > 50) true else false
-    println(isHot)
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
 }
