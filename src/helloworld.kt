42a14546ebb9fb7ccaa490d07e4ccf35ddc4c63a
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 14:24:37 +0600

    5.2.5

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,13 +1,12 @@
 fun main() {
     val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
-
-    // eager, creates a new list
-    val eager = decorations.filter { it[0] == 'p' }
-    println("eager: $eager")
-    // lazy, will wait until asked to evaluate
-    val filtered = decorations.asSequence().filter { it[0] == 'p' }
-    println("filtered: $filtered")
-    // force evaluation of the lazy list
-    val newList = filtered.toList()
-    println("new list: $newList")
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
 }
