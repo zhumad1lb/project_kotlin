71b9ec0edc37e7f9a9dc8db42885fab5260ac71f
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 13:37:32 +0600

    4.

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -2,15 +2,19 @@
  fun main(args: Array<String>) {
      feedTheFish()
  }
+ fun isTooHot(temperature: Int) = temperature > 30
+
+ fun isDirty(dirty: Int) = dirty > 30
+
+ fun isSunday(day: String) = day == "Sunday"
  fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
      return when {
-         temperature > 30 -> true
-         dirty > 30 -> true
-         day == "Sunday" ->  true
-         else -> false
+         isTooHot(temperature) -> true
+         isDirty(dirty) -> true
+         isSunday(day) -> true
+         else  -> false
      }
  }
-
  fun feedTheFish() {
      val day = randomDay()
      val food = fishFood(day)
