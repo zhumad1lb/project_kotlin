d9fb15ee2334c57d978e23de1b2123ed07561e73
zhumad1lb <bagdat.13.zhumadil@gmail.com>
2022-09-21 13:21:52 +0600

    3.2.3

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,27 +1,28 @@
 import java.util.*
-fun feedTheFish() {
+
+ fun randomDay() : String {
+     val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+         "Friday", "Saturday", "Sunday")
+     return week[Random().nextInt(week.size)]
+ }
+
+ fun fishFood (day : String) : String {
+     return when (day) {
+         "Monday" -> "flakes"
+         "Wednesday" -> "redworms"
+         "Thursday" -> "granules"
+         "Friday" -> "mosquitoes"
+         "Sunday" -> "plankton"
+         else -> "nothing"
+     }
+ }
+
+ fun feedTheFish() {
      val day = randomDay()
      val food = fishFood(day)
-
      println ("Today is $day and the fish eat $food")
  }
- fun fishFood (day : String) : String {
-     val food : String
-     when (day) {
-         "Monday" -> food = "flakes"
-         "Wednesday" -> food = "redworms"
-         "Thursday" -> food = "granules"
-         "Friday" -> food = "mosquitoes"
-         "Sunday" -> food = "plankton"
-         else -> food = "nothing"
-     }
-     return food
- }
+
  fun main(args: Array<String>) {
      feedTheFish()
- }
- fun randomDay(): String {
-     var week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday",
-         "Friday", "Saturday", "Sunday")
-     return week[java.util.Random().nextInt(week.size)]
  }
